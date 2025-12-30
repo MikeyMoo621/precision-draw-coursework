3b0adcaf6b1ce92e4109b2fd20ce914df3437b1f
MikeyMoo621 <OByrne-M1@ulster.ac.uk>
2025-12-30 12:49:55 +0000

    Did not start main yesterday due to commitments however have added that now.

---
+++ src/precisiondraw/Main.java
@@ -0,0 +1,11 @@
+package precisiondraw;
+
+public class Main {
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        System.out.println("Testing deck draw:");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(deck.draw());
+        }
+    }
+}
